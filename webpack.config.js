dffbdb0b57c8c6bd029a11239426c5179b143504
peimanhp <peiman_hp@yahoo.com>
2023-03-24 02:04:14 +0430

    header skeleton created

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/i,
+        test: /\.(s[ac]|c)ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
       },      
       {
