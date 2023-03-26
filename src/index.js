782d59f27657fb1b31c7bc43eacda16f143a464b
peimanhp <peiman_hp@yahoo.com>
2023-03-27 00:55:16 +0430

    transition on load page

--- src/index.js
+++ src/index.js
@@ -11,126 +11,148 @@ header.classList.add("header");
 const homeBtn = document.createElement("button");
 homeBtn.id = "home";
 homeBtn.innerText = "Home";
+homeBtn.addEventListener("click", showHomePage);
 
 const menuBtn = document.createElement("button");
 menuBtn.id = "menu";
 menuBtn.innerText = "Menu";
+menuBtn.addEventListener("click", showMenuPage);
 
 const aboutBtn = document.createElement("button");
 aboutBtn.id = "about";
 aboutBtn.innerText = "About";
+aboutBtn.addEventListener("click", showAboutPage);
 
 content.appendChild(header);
 header.appendChild(homeBtn);
 header.appendChild(menuBtn);
 header.appendChild(aboutBtn);
 
-// Menu
+// Menu page
 
 const pageContent = document.createElement("div");
-pageContent.classList = "page-content";
+pageContent.classList.add("page-content");
 content.appendChild(pageContent);
 
-const wrapperMenu = document.createElement("div");
-wrapperMenu.classList.add("wrapper-menu");
-pageContent.appendChild(wrapperMenu);
+function showMenuPage() {
+  pageContent.innerHTML = "";
+  pageContent.classList.add("show");
 
-const ul = document.createElement("ul");
-ul.classList.add("grid");
-wrapperMenu.appendChild(ul);
+  const wrapperMenu = document.createElement("div");
+  wrapperMenu.classList.add("wrapper-menu");
+  pageContent.appendChild(wrapperMenu);
 
-function addItem(name, recepies, prices, imageSource) {
-  const item1 = document.createElement("li");
-  ul.appendChild(item1);
+  const ul = document.createElement("ul");
+  ul.classList.add("grid");
+  wrapperMenu.appendChild(ul);
 
-  const card1 = document.createElement("div");
-  card1.classList.add("card");
-  item1.appendChild(card1);
+  function addItem(name, recepies, prices, imageSource) {
+    const item1 = document.createElement("li");
+    ul.appendChild(item1);
 
-  const img1 = document.createElement("img");
-  img1.setAttribute("src", imageSource);
-  card1.appendChild(img1);
+    const card1 = document.createElement("div");
+    card1.classList.add("card");
+    item1.appendChild(card1);
 
-  const cardContent = document.createElement("div");
-  cardContent.classList.add("card-content");
-  card1.appendChild(cardContent);
+    const img1 = document.createElement("img");
+    img1.setAttribute("src", imageSource);
+    card1.appendChild(img1);
 
-  const cardTitle = document.createElement("h2");
-  cardTitle.classList.add("card-title");
-  cardTitle.innerText = name;
-  cardContent.appendChild(cardTitle);
+    const cardContent = document.createElement("div");
+    cardContent.classList.add("card-content");
+    card1.appendChild(cardContent);
 
-  const recepie = document.createElement("p");
-  recepie.innerText = recepies;
-  cardContent.appendChild(recepie);
+    const cardTitle = document.createElement("h2");
+    cardTitle.classList.add("card-title");
+    cardTitle.innerText = name;
+    cardContent.appendChild(cardTitle);
 
-  const price = document.createElement("p");
-  price.innerText = prices;
-  cardContent.appendChild(price);
-}
+    const recepie = document.createElement("p");
+    recepie.innerText = recepies;
+    cardContent.appendChild(recepie);
 
-class Food {
-  constructor(name, recepie, price, imageSource) {
-    this.name = name;
-    this.recepie = recepie;
-    this.price = price;
-    this.imageSource = imageSource;
-    addItem(this.name, this.recepie, this.price, this.imageSource);
+    const price = document.createElement("p");
+    price.innerText = prices;
+    cardContent.appendChild(price);
   }
+
+  class Food {
+    constructor(name, recepie, price, imageSource) {
+      this.name = name;
+      this.recepie = recepie;
+      this.price = price;
+      this.imageSource = imageSource;
+      addItem(this.name, this.recepie, this.price, this.imageSource);
+    }
+  }
+
+  const brisket = new Food(
+    "Brisket Pizza",
+    "Tomato sauce + mozzarella cheese + mushroom + veal fillet + onion + olive + rocket + oregano",
+    "Price: 20$",
+    "../src/images/brisket-normal.jpg"
+  );
+
+  const chickenPesto = new Food(
+    "Chicken Pesto Pizza",
+    "Pesto sauce + mozzarella cheese + spinach + mushrooms + smoked chicken breast + olives + dried tomatoes",
+    "Price: 22$",
+    "../src/images/chicken-pesto-normal.jpg"
+  );
+
+  const jamaicanChicken = new Food(
+    "Jamaican Chicken Pizza",
+    "Tomato sauce + mozzarella cheese + mushroom + Jamaican chicken breast (spicy) + onion + olive",
+    "Price: 23$",
+    "../src/images/jamaican-chicken-normal.jpg"
+  );
+
+  const margherita = new Food(
+    "Margherita Pizza",
+    "Tomato sauce + mozzarella cheese + cottage mozzarella cheese + Italian basil + oregano",
+    "Price: 21$",
+    "../src/images/margherita-normal.jpg"
+  );
+
+  const quattroFormaggi = new Food(
+    "Quattro Formaggi Pizza",
+    "Tomato sauce + mozzarella cheese + blue cheese + buttered cheese + white cheese + oregano",
+    "Price: 25$",
+    "../src/images/quattro-formaggi-normal.jpg"
+  );
+
+  const salami = new Food(
+    "Salami Pizza",
+    "Tomato sauce + mozzarella cheese + salami + oregano",
+    "Price: 22$",
+    "../src/images/salami-normal.jpg"
+  );
+
+  const turkey = new Food(
+    "Turkey Pizza",
+    "Tomato sauce + mozzarella cheese + turkey bacon + mushroom + olive + oregano",
+    "Price: 25$",
+    "../src/images/turkey-normal.jpg"
+  );
+
+  const vegetables = new Food(
+    "Vegetables Pizza",
+    "Tomato sauce + mozzarella cheese + seasoned zucchini and eggplant + spinach + mushroom + onion + cherry tomato + bell pepper + corn + oregano",
+    "Price: 23$",
+    "../src/images/vegetables-normal.jpg"
+  );
 }
 
-const brisket = new Food(
-  "Brisket Pizza",
-  "Tomato sauce + mozzarella cheese + mushroom + veal fillet + onion + olive + rocket + oregano",
-  "Price: 20$",
-  "../src/images/brisket-normal.jpg"
-);
+// Home page
 
-const chickenPesto = new Food(
-  "Chicken Pesto Pizza",
-  "Pesto sauce + mozzarella cheese + spinach + mushrooms + smoked chicken breast + olives + dried tomatoes",
-  "Price: 22$",
-  "../src/images/chicken-pesto-normal.jpg"
-);
+function showHomePage() {
+  pageContent.innerHTML = "";
+  pageContent.classList.remove("show");
+}
 
-const jamaicanChicken = new Food(
-  "Jamaican Chicken Pizza",
-  "Tomato sauce + mozzarella cheese + mushroom + Jamaican chicken breast (spicy) + onion + olive",
-  "Price: 23$",
-  "../src/images/jamaican-chicken-normal.jpg"
-);
+// About page
 
-const margherita = new Food(
-  "Margherita Pizza",
-  "Tomato sauce + mozzarella cheese + cottage mozzarella cheese + Italian basil + oregano",
-  "Price: 21$",
-  "../src/images/margherita-normal.jpg"
-);
-
-const quattroFormaggi = new Food(
-  "Quattro Formaggi Pizza",
-  "Tomato sauce + mozzarella cheese + blue cheese + buttered cheese + white cheese + oregano",
-  "Price: 25$",
-  "../src/images/quattro-formaggi-normal.jpg"
-);
-
-const salami = new Food(
-  "Salami Pizza",
-  "Tomato sauce + mozzarella cheese + salami + oregano",
-  "Price: 22$",
-  "../src/images/salami-normal.jpg"
-);
-
-const turkey = new Food(
-  "Turkey Pizza",
-  "Tomato sauce + mozzarella cheese + turkey bacon + mushroom + olive + oregano",
-  "Price: 25$",
-  "../src/images/turkey-normal.jpg"
-);
-
-const vegetables = new Food(
-  "Vegetables Pizza",
-  "Tomato sauce + mozzarella cheese + seasoned zucchini and eggplant + spinach + mushroom + onion + cherry tomato + bell pepper + corn + oregano",
-  "Price: 23$",
-  "../src/images/vegetables-normal.jpg"
-);
+function showAboutPage() {
+  pageContent.innerHTML = "";
+  pageContent.classList.remove("show");
+}
